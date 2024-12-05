aa71c6036daf0fcf3b1fedd41d86efc47bfb2735
PAR23006622 <par23006622@heartofyorkshire.ac.uk>
2024-12-05 23:24:45 +0000

    v0.9.1

--- lib/utils/detect.ts
+++ lib/utils/detect.ts
@@ -1,11 +1,11 @@
-"use client";
-
-import { detect } from 'detect-browser';
+// formula-1-fantasy/lib/utils/detect.ts
+import { detect, BrowserInfo } from 'detect-browser';
 
 export const isMobile = () => {
   const browser = detect();
-  if (browser) {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(browser.ua);
+  if (browser && 'ua' in browser && typeof browser.ua === 'string') { // Check if 'ua' exists and is a string
+    const userAgent = browser.ua; // Now safely treated as a string
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
   }
   return false;
 };
